54db9d1d0400bc069529011930a81f7d0eda116e
JEMurcia <jossie.murcia@hotmail.com>
2017-10-21 21:29:50 -0500

    Added User Registration and login 	- A Class Called User was created for future connection with accounts registers 	- The CSS Called 'style' was added 	- The HTML files for SignUp an LogIn was initialized just for view

---
+++ src/main/java/edu/eci/arsw/game/model/User.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.game.model;
+
+/**
+ *
+ * @author Esteban
+ */
+public class User {
+    private Long id;
+    private String nickname;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
