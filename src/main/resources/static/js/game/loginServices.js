cb47026f7a4e7d539393f625fa8f861d644f47a0
Zekkenn <sebastian.moreno-r@mail.escuelaing.edu.co>
2017-10-27 12:06:45 -0500

    view - sign up realized

---
+++ src/main/resources/static/js/game/loginServices.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var loginServices = ( function(){
+    var id = 0;
+    var nickName = "";
+    var email = "";
+    var passw = "";
+    
+    return{
+        signUp : function(){
+            
+        }
+    }
+    
+})();
